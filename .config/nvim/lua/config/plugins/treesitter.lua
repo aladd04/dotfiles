f73d2bad5c3d4c370c0874bca25f03cb55130481
Andy Ladd <aladd@shiftdigital.com>
2024-11-22 11:44:48 -0500

    treesitter

---
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -0,0 +1,72 @@
+return {
+  "nvim-treesitter/nvim-treesitter", -- https://github.com/nvim-treesitter/nvim-treesitter
+  build = ":TSUpdate", -- installs or updates parsers
+  event = { "BufReadPre", "BufNewFile" }, -- load before a file is put into the buffer
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      sync_install = false, -- allow parsers to be installed in the background
+      incremental_selection = { -- allows selecting "blocks" or "scopes" of code using keybinds
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+      ensure_installed = {
+        "angular",
+        "bash",
+        "c",
+        "c_sharp",
+        "comment",
+        "cpp",
+        "csv",
+        "diff",
+        "dockerfile",
+        "editorconfig",
+        "gitignore",
+        "go",
+        "graphql",
+        "helm",
+        "html",
+        "http",
+        "java",
+        "javascript",
+        "json",
+        "kusto",
+        "lua",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "mermaid",
+        "nginx",
+        "pem",
+        "powershell",
+        "printf",
+        "python",
+        "regex",
+        "sql",
+        "ssh_config",
+        "swift",
+        "terraform",
+        "tmux",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      },
+    })
+  end
+}
