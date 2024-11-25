ad48c13f25336207db96cbae8b9a7dd016838b58
Andy Ladd <aladd04@gmail.com>
2024-11-25 00:10:40 -0500

    trying to get dotnet working

--- .config/nvim/lua/config/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/config/plugins/lsp/lspconfig.lua
@@ -49,6 +49,37 @@ return {
           capabilities = capabilities,
         })
       end,
+      -- configuration from https://github.com/tris203/rzls.nvim
+      ["roslyn"] = function()
+        lspconfig["roslyn"].setup({
+          config = {
+            capabilities = capabilities,
+            on_attach = require("lspattach"),
+            handlers = require("rzls.roslyn_handlers")
+          },
+          args = {
+            "--logLevel=Information",
+            "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+            "--razorSourceGenerator=" .. vim.fs.joinpath(
+              vim.fn.stdpath "data" --[[@as string]],
+              "mason",
+              "packages",
+              "roslyn",
+              "libexec",
+              "Microsoft.CodeAnalysis.Razor.Compiler.dll"
+            ),
+            "--razorDesignTimePath=" .. vim.fs.joinpath(
+              vim.fn.stdpath "data" --[[@as string]],
+              "mason",
+              "packages",
+              "rzls",
+              "libexec",
+              "Targets",
+              "Microsoft.NET.Sdk.Razor.DesignTime.targets"
+            ),
+          },
+        })
+      end,
     })
   end
 }
--- .config/nvim/lua/config/plugins/lsp/mason.lua
+++ .config/nvim/lua/config/plugins/lsp/mason.lua
@@ -2,12 +2,20 @@ return {
   "williamboman/mason.nvim", -- https://github.com/williamboman/mason.nvim
   dependencies = {
     "williamboman/mason-lspconfig.nvim", -- https://github.com/williamboman/mason-lspconfig.nvim
+    -- "tris203/rzls.nvim", -- https://github.com/tris203/rzls.nvim
+    -- "seblj/roslyn.nvim", -- https://github.com/seblj/roslyn.nvim
   },
   config = function()
     local mason = require("mason")
     local mason_lsp = require("mason-lspconfig")
 
-    mason.setup({})
+    mason.setup({
+      registries = {
+        "github:mason-org/mason-registry", -- default registry
+        "github:Crashdummyy/mason-registry", -- dotnet registry for roslyn and rzls https://github.com/Crashdummyy/mason-registry
+        -- "github:seblj/roslyn-mason-registry", -- dotnet registry for roslny, need this too for some reason
+      },
+    })
 
     mason_lsp.setup({
       -- language servers to install or use, many may have pre-reqs to install in order to use
@@ -28,7 +36,9 @@ return {
         -- "nginx_language_server",
         "powershell_es",
         "pyright",
+        "roslyn",
         "rust_analyzer",
+        -- "rzls",
         "sqls",
         "terraformls",
         "taplo",
