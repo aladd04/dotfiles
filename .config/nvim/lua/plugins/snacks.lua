744c7ddfdc4b9f271ab0aa4b45b3c987766e10db
Andy Ladd <aladd@shiftdigital.com>
2025-05-19 12:18:38 -0400

    show hidden files

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+-- found this issue that explained how to show hidden files by default https://github.com/LazyVim/LazyVim/discussions/5559
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      hidden = true,
+      ignored = true,
+      -- exclude = {
+      --   "node_modules",
+      --   ".git",
+      -- },
+    },
+  },
+}
