2bab20345f70d2db1a33852e6e6f9721740246b2
Andy Ladd <aladd04@gmail.com>
2025-05-18 22:49:12 -0400

    Lazyvim extras (#5)
    * trying extras
    
    * langs
    
    * better paste
    
    * vscode

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -6,25 +6,10 @@ return {
       "github:Crashdummyy/mason-registry",
     },
     ensure_installed = {
-      "angular-language-server",
       "bash-language-server",
-      "clangd",
-      "css-lsp",
-      "dockerfile-language-server",
-      "gopls",
-      "html-lsp",
-      "typescript-language-server",
-      "json-lsp",
-      "lua-language-server",
-      "markdown-oxide",
-      "powershell-editor-services",
-      "pyright",
-      "rust-analyzer",
-      "sqls",
-      "terraform-ls",
-      "taplo", -- toml
-      "vim-language-server",
       "lemminx", -- xml
+      "powershell-editor-services",
+      "roslyn",
     },
   },
 }
