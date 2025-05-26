747a485c68e22c14efe18d42f7c384966258ed1a
Andy Ladd <aladd04@gmail.com>
2025-05-25 23:54:56 -0400

    dotnet zsh autocompletions

--- .aladd.zsh
+++ .aladd.zsh
@@ -331,6 +331,26 @@ zstyle ':fzf-tab:complete:cd:*' popup-pad 30 0
 
 # 1password cli auto completion
 eval "$(op completion zsh)"
+
+# zsh parameter completion for the dotnet CLI
+_dotnet_zsh_complete()
+{
+  local completions=("$(dotnet complete "$words")")
+
+  # If the completion list is empty, just continue with filename selection
+  if [ -z "$completions" ]
+  then
+    _arguments '*::arguments: _normal'
+    return
+  fi
+
+  # This is not a variable assignment, don't remove spaces!
+  _values = "${(ps:\n:)completions}"
+}
+
+# dotnet
+compdef _dotnet_zsh_complete dotnet
+# 1passord
 compdef _op op
 
 # exoprts that got added by tools I installed
