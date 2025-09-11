6da86fa63e010ef4325d60c5e2b898328e45da12
Andy Ladd <aladd04@gmail.com>
2025-09-11 01:29:45 -0400

    tmux setup with wezterm (incompatible!)

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,4 +11,39 @@ config.color_scheme = "Catppuccin Mocha"
 -- config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 
+-- similar setup to my tmux
+config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 5000 }
+config.keys = {
+	{
+		key = "\\",
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "-",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "h",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+}
+
 return config
