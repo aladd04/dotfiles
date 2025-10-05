042da199f930f7076049fb15f33095e8b2d86fc4
Andy Ladd <aladd04@gmail.com>
2025-10-04 22:32:19 -0400

    that fix didn't work

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -2,18 +2,9 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
-    routes = {
-      {
-        -- fixes https://github.com/folke/noice.nvim/issues/1150
-        filter = {
-          event = "lsp",
-          kind = "progress",
-          cond = function(message)
-            local client = vim.tbl_get(message.opts, "progress", "client")
-            return client == "roslyn"
-          end,
-        },
-        opts = { skip = true },
+    lsp = {
+      progress = {
+        enabled = false, -- temp fix for https://github.com/folke/noice.nvim/issues/1150
       },
     },
   },
